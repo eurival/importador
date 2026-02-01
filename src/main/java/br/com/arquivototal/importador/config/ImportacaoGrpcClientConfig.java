cc118606cb95dd9bc7981dedf0389501c7f670f1
Eurival Bernardino da S.Gomes <eurival_b@hotmail.com>
2026-02-01 17:50:34 -0300

    corrigir concorrencia

--- src/main/java/br/com/arquivototal/importador/config/ImportacaoGrpcClientConfig.java
+++ src/main/java/br/com/arquivototal/importador/config/ImportacaoGrpcClientConfig.java
@@ -3,6 +3,9 @@ package br.com.arquivototal.importador.config;
 import br.com.arquivototal.gedtotalapi.grpc.ImportacaoGedServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +21,8 @@ public class ImportacaoGrpcClientConfig {
         // Se o host vier como "dns:///...", o gRPC usará o NameResolver nativo
         return ManagedChannelBuilder.forTarget(host + ":" + port)
             .defaultLoadBalancingPolicy("round_robin") 
+            .keepAliveTime(30, TimeUnit.SECONDS)  
+            .keepAliveTimeout(10, TimeUnit.SECONDS)
             .usePlaintext()
             .build();
     }
